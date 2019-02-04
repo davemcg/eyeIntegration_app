22215e2ac71a896c9119f5861e6db07e560b0c92
David McGaughey <mcgaughey@gmail.com>
2019-02-04 14:33:11 -0500

    adds options to exclude sqlite file from tar

--- R/data_compressor.R
+++ R/data_compressor.R
@@ -4,12 +4,17 @@
 #' 
 #' @param www_location The path which contains the www/ App folder
 #' @param tar_name path/name_of_tar_file.tar.gz
+#' @param include_DB Should the SQLite databases be included?
 #' 
 #' @examples
 #' \dontrun{tar_maker(www_location = paste0(system.file('app', 
 #' package = 'eyeIntegrationApp')), tar_name = 'eyeIntegration_v100_01.tar.gz') }
 
-data_compressor <- function(www_location, tar_name = '~/Desktop/eyeIntegration_v100_02.tar.gz'){
+data_compressor <- function(www_location, tar_name = '~/Desktop/eyeIntegration_v100_02.tar.gz', include_DB = T){
   setwd(www_location)
-  system(paste('tar --exclude=".*" -cvf - www/ | pigz >', tar_name))
+  if (include_DB){
+    system(paste('tar --exclude=".*|" -cvf - www/ | pigz >', tar_name))
+  } else {
+    system(paste('tar --exclude=".*|" --exclude="*sqlite" -cvf - www/ | pigz >', tar_name)) 
+  }
 }
